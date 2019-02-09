a13822a8b34fdb70f48e86b1038d28b4a6f28528
engamin <abdelrahman.amin93@gmail.com>
2019-02-09 21:39:46 +0200

    add edit for profile page

--- Views/member/edit.php
+++ Views/member/edit.php
@@ -1,3 +1,42 @@
 <?php
-echo "Edit Profile Page";
+
+$db = new MySQLHandler('members');
+$user = $db->get_record_by_id($_SESSION['user_id'])[0];
+
+
+if(isset($_POST['update'])){    
+    $db = new MySQLHandler('members');
+    $edited_values = array(
+        "fullname"=>$_POST['fullname'],
+        "job"=>$_POST['job']
+        // "password"=>$_POST['passwrod'],
+        // "cv"=>$_POST['cv'],
+        // "photo"=>$_POST['photo']
+     );
+
+    $user = $db->update($edited_values, intval($_SESSION['user_id']) );
+    header('Location: index.php');
+
+    echo "$user";
+}
+
 ?>
+
+
+<h4>Edit Profile</h4>
+
+<form   method = "post" enctype="multipart/form-data">
+    <label>UserName  :</label>
+    <input type = "text" name = "username" value = "<?php echo $user['username']; ?> "class = "box" placeholder="Enter your Name" /><br /><br />
+    <label>Password  :</label>
+    <input type = "password" name = "password" value = "" class = "box" placeholder="Enter your PassWord" /><br/><br />
+    <label>Full Name  :</label>
+    <input type = "text" name = "fullname" value = "<?php echo $user['fullname'];?> " class = "box" placeholder="Enter your Full Name" /><br/><br />
+    <label>Job Title  :</label>
+    <input type = "text" name = "job" class = "box" value = "<?php echo $user['job']; ?>" placeholder="Enter your Job Title" /><br/><br />
+    <label>Upload photo</label>
+    <input type="file"   name = "photo"  ><br />
+    <label>Upload cv</label>
+    <input type="file"  name = "cv" > <br />
+    <input  type = "submit" name = "update" value="update"><br />
+</form>	
--- Views/member/profile.php
+++ Views/member/profile.php
@@ -1,16 +1,41 @@
 <?php
-echo "View Profile Page";
 
 $db = new MySQLHandler('members');
 $user = $db->get_record_by_id($_SESSION['user_id'])[0];
 
-echo '<pre>' . var_export($user, true) . '</pre>';
 ?>
 
-<div>
-    // Put LOGIC OF PROFILE HERE 
-    
-    
-    
-    
-</div>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+
+        <div align = "center">
+            <div style = "width:600px; border: solid 1px #333333; " align = "left">
+                <div align="center" style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b><?php  echo $user['fullname']." "."Profile" ?></b></div>	
+                <div style = "margin:30px">
+                    <div align="right" >
+                        <!-- <embed src="<?php echo "cvs/". $user['cv']; ?>" width="300" height="375"  type="application/pdf"> -->
+                         <a href="<?= 'cvs/'. $user['cv']; ?>" target='_blank' ><span>View CV</span></a>
+                         
+                    </div>
+                    <div  width=300px; >
+                        <p ><img src="<?php echo "photos/". $user['photo']; ?>" style="height:106px;width:106px" alt="Avatar"></p>
+                        <br>
+                        <strong><i >NAME : </i> <?php  echo $user['fullname']; ?></strong>
+                        <br>
+                        <strong><i >JOB TITLE: </i> <?php  echo $user['job']; ?></strong>
+                        <p><i ></i> London, UK</p>
+                        <p><i ></i> April 1, 1988</p>
+                        <a href="?edit">Edit </a> 
+                    </div>
+                </div>
+                <br>
+            </div>
+        </div>
+
+    </body>
+</html>
